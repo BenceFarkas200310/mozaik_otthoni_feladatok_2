2a552405a35c257219d27560187ce1c9fedb3717
Bence Farkas <bence.farkas2003@gmail.com>
2025-04-29 12:36:49 +0200

    Create migration files

---
+++ esemeny-kezelo/database/migrations/2025_04_29_103018_create_visible_to_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('visible_to', function (Blueprint $table) {
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->primary(['event_id', 'user_id']);
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('visible_to');
+    }
+};
