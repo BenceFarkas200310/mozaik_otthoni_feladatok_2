da6947172ea19cf86ccd08b9bfbcdb34165c5fdc
Bence Farkas <bence.farkas2003@gmail.com>
2025-05-09 16:26:12 +0200

    Add date to event cards

--- esemeny-kezelo/resources/views/components/event-card.blade.php
+++ esemeny-kezelo/resources/views/components/event-card.blade.php
@@ -3,7 +3,13 @@
     <div class="card-body">
     <h5 class="card-title">{{$event->name}}</h5>
     <x-badge :event="$event" />
-    <p class="card-text">{{Str::limit($event->description, 80)}}</p>
+    <p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-event" viewBox="0 0 16 16">
+            <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+          </svg>
+          {{\Carbon\Carbon::parse($event->date)->format('Y-m-d H:i')}}
+    </p>
     <p>
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
             <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
@@ -11,6 +17,7 @@
           </svg>
           {{$event->location}}
     </p>
+    <p class="card-text">{{Str::limit($event->description, 80)}}</p>
     <a href="/event/{{$event->id}}" class="btn btn-primary">Megnézem</a>
     </div>
 </div>
